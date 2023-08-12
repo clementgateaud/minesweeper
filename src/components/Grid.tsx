bd5a15437d4af62a8671fea8d2341a4c79146bae
Clément Gateaud <clement.gateaud@gmail.com>
2023-08-12 17:12:39 +0200

    fix margin issue

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -4,7 +4,7 @@ import styles from "./Grid.module.css";
 import { useAppContext } from "../providers/AppProvider";
 
 export const Grid: FunctionComponent = () => {
-  const { grid, rowsNumber } = useAppContext();
+  const { grid } = useAppContext();
   return (
     <div className={styles.main}>
       {grid.map((row, rowIndex) => {
@@ -12,7 +12,7 @@ export const Grid: FunctionComponent = () => {
           <div
             key={rowIndex}
             className={styles.row}
-            style={{ height: `${100 / rowsNumber}%` }}
+            style={{ height: `${100 / grid.length}%` }}
           >
             {row.map((cell) => {
               return <Cell key={cell.id} cell={cell} />;
