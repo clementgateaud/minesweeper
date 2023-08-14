ce5c9ba6a8c2bfaf705884df003bf6d40d5dbd66
Clément Gateaud <clement.gateaud@gmail.com>
2023-08-14 21:29:25 +0200

    improve game settings style

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -78,55 +78,58 @@ export const Home: FunctionComponent = () => {
       <div>
         <h1>Minesweeper</h1>
         <div className={styles.gameSettings}>
-          <div>
-            <label htmlFor="gridSize">Grid size: </label>
-            <select
-              id="gridSize"
-              onChange={(event) => handleGridSizeChange(event)}
-              value={gridSizeInput.toString()}
-            >
-              <option value="50">50</option>
-              <option value="100">100</option>
-              <option value="150">150</option>
-              <option value="200">200</option>
-              <option value="250">250</option>
-              <option value="300">300</option>
-              <option value="350">350</option>
-              <option value="400">400</option>
-              <option value="450">450</option>
-              <option value="500">500</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="minesDensity">Mines density: </label>
-            <select
-              id="minesDensity"
-              onChange={(event) => handleMinesDensityChange(event)}
-              value={minesDensityInput.toString()}
-            >
-              <option value="0.05">5%</option>
-              <option value="0.10">10%</option>
-              <option value="0.15">15%</option>
-              <option value="0.20">20%</option>
-              <option value="0.25">25%</option>
-              <option value="0.30">30%</option>
-              <option value="0.35">35%</option>
-              <option value="0.40">40%</option>
-              <option value="0.45">45%</option>
-              <option value="0.50">50%</option>
-              <option value="0.55">55%</option>
-              <option value="0.60">60%</option>
-              <option value="0.65">65%</option>
-              <option value="0.70">70%</option>
-              <option value="0.75">75%</option>
-              <option value="0.80">80%</option>
-              <option value="0.85">85%</option>
-              <option value="0.90">90%</option>
-              <option value="0.95">95%</option>
-            </select>
+          <div className={styles.gameSettingsInput}>
+            <div className={styles.gridSize}>
+              <label htmlFor="gridSize">Grid size: </label>
+              <select
+                id="gridSize"
+                onChange={(event) => handleGridSizeChange(event)}
+                value={gridSizeInput.toString()}
+              >
+                <option value="50">50</option>
+                <option value="100">100</option>
+                <option value="150">150</option>
+                <option value="200">200</option>
+                <option value="250">250</option>
+                <option value="300">300</option>
+                <option value="350">350</option>
+                <option value="400">400</option>
+                <option value="450">450</option>
+                <option value="500">500</option>
+              </select>
+            </div>
+            <div className={styles.minesDensity}>
+              <label htmlFor="minesDensity">Mines density: </label>
+              <select
+                id="minesDensity"
+                onChange={(event) => handleMinesDensityChange(event)}
+                value={minesDensityInput.toString()}
+              >
+                <option value="0.05">5%</option>
+                <option value="0.10">10%</option>
+                <option value="0.15">15%</option>
+                <option value="0.20">20%</option>
+                <option value="0.25">25%</option>
+                <option value="0.30">30%</option>
+                <option value="0.35">35%</option>
+                <option value="0.40">40%</option>
+                <option value="0.45">45%</option>
+                <option value="0.50">50%</option>
+                <option value="0.55">55%</option>
+                <option value="0.60">60%</option>
+                <option value="0.65">65%</option>
+                <option value="0.70">70%</option>
+                <option value="0.75">75%</option>
+                <option value="0.80">80%</option>
+                <option value="0.85">85%</option>
+                <option value="0.90">90%</option>
+                <option value="0.95">95%</option>
+              </select>
+            </div>
           </div>
           <button
             onClick={() => handleStartGame(gridSizeInput, minesDensityInput)}
+            className={styles.button}
           >
             Start new game
           </button>
